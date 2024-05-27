8c65e7d131396d4cc0871b5136a21513de3c1042
Florin Milu <fmilu87@gmail.com>
2024-05-27 22:08:33 +0300

    Implemented Markers and polylines

---
+++ lib/pages/traceroute/map_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class MapScreen extends StatefulWidget {
+  final List<String> ipAddresses;
+
+  MapScreen({required this.ipAddresses});
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  late MapController mapController;
+  List<Marker> markers = [];
+  List<LatLng> points = [];
+  List<Polyline> polylines = [];
+  var latitude = 51.509364;
+  var longitude = -0.128928;
+
+  @override
+  void initState() {
+    super.initState();
+    mapController = MapController();
+    _getIpLocations();
+  }
+
+  Future<void> _waitForFiveSeconds() async {
+    await Future.delayed(Duration(seconds: 7));
+  }
+
+  Future<void> _getIpLocations() async {
+    for (String ip in widget.ipAddresses) {
+      try {
+        final location = await fetchLocation(ip);
+        latitude = location['lat'];
+        longitude = location['lon'];
+
+        setState(() {
+          markers.add(
+            Marker(
+              width: 80.0,
+              height: 80.0,
+              point: LatLng(latitude, longitude),
+              child: Icon(Icons.location_pin, color: Colors.red, size: 40),
+            ),
+          );
+          points.add(LatLng(latitude, longitude));
+        });
+      } catch (e) {
+        print('Failed to fetch location for IP: $ip');
+      }
+    }
+    setState(() {
+      polylines.add(Polyline(points: points, color: Colors.red));
+    });
+  }
+
+  Future<Map<String, dynamic>> fetchLocation(String ip) async {
+    final response = await http.get(Uri.parse('http://ip-api.com/json/${ip}'));
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load location');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _waitForFiveSeconds(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Map'),
+              ),
+              body: FlutterMap(
+                options: MapOptions(
+                  initialCenter: LatLng(latitude, longitude),
+                  initialZoom: 9.2,
+                ),
+                children: [
+                  TileLayer(
+                    urlTemplate:
+                        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    userAgentPackageName: 'com.example.app',
+                  ),
+                  MarkerLayer(markers: markers),
+                  PolylineLayer(polylines: polylines)
+                ],
+              ),
+            );
+          }
+        });
+  }
+}
--- lib/pages/traceroute/traceroute_page.dart
+++ lib/pages/traceroute/traceroute_page.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:NetProbe/base_page.dart';
+import 'package:NetProbe/pages/traceroute/map_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_traceroute/flutter_traceroute.dart';
 import 'package:flutter_traceroute/flutter_traceroute_platform_interface.dart';
+import 'package:NetProbe/ui/popular_chip.dart';
 
 class TraceScreen extends StatefulWidget {
   static const defaultDNS = '8.8.8.8';
@@ -14,43 +19,42 @@ class TraceScreen extends StatefulWidget {
 
 class _TraceScreenState extends BasePage<TraceScreen> {
   List<TracerouteStep> traceResults = [];
-  
+  List<String> ipv4addresses = [];
   late final FlutterTraceroute traceroute;
   late final TextEditingController hostController;
   bool start = true;
 
   @override
   void initState() {
-    super.initState();    
+    super.initState();
     traceroute = FlutterTraceroute();
-    
   }
 
   void onTrace() {
     start = true;
     traceroute.stopTrace();
     setState(() {
-      
       traceResults = <TracerouteStep>[];
     });
 
-    final host = textEditingController.text;    
+    final host = textEditingController.text;
     const ttl = TracerouteArgs.ttlDefault;
 
     final args = TracerouteArgs(host: host, ttl: ttl);
 
     traceroute.trace(args).listen(
-    (event) {
-      setState(() {
-        traceResults = List<TracerouteStep>.from(traceResults)..add(event);
-      });
-    },
-    onError: (error) {
-      setState(() {
-        traceResults.add(TracerouteStepFailed('Failed to resolve host: $host'));
-      });
-    },
-  );
+      (event) {
+        setState(() {
+          traceResults = List<TracerouteStep>.from(traceResults)..add(event);
+        });
+      },
+      onError: (error) {
+        setState(() {
+          traceResults
+              .add(TracerouteStepFailed('Failed to resolve host: $host'));
+        });
+      },
+    );
   }
 
   void onStop() {
@@ -58,62 +62,153 @@ class _TraceScreenState extends BasePage<TraceScreen> {
     traceroute.stopTrace();
 
     setState(() {
-      traceResults = <TracerouteStep>[];
+      traceResults = <TracerouteStep>[];      
     });
   }
 
-  
+  Widget _getDomainChip(String label) {
+    return PopularChip(
+      label: label,
+      onPressed: () {
+        textEditingController.text = label;
+      },
+    );
+  }
+
+  @override
+  Widget buildPopularChips() {
+    return Card(
+      child: ListTile(
+        title: const Text('Popular targets'),
+        subtitle: Wrap(
+          children: [
+            _getDomainChip('discord.com'),
+            _getDomainChip('emag.ro'),
+            _getDomainChip('wallmart.com'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>  MapScreen(ipAddresses: ipv4addresses,),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  shadowColor: Colors.blueAccent // Adjust padding as needed
+                  ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Show on Map',
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.secondary),
+                  ),
+                  SizedBox(width: 8.0), // Space between the text and the icon
+                  Icon(
+                    Icons.map,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget buildResults(BuildContext context) {
     // TODO: implement buildResults
-      return traceResults.isEmpty
+    return traceResults.isEmpty
         ? const Center(
             child: Text(
               'No addresses found yet.\nAll addresses will appear here.',
               textAlign: TextAlign.center,
             ),
           )
-        : ListView(              
-              children: [
-                for (final result in traceResults)
-                  Text(
-                    result.toString(),
-                    style: TextStyle(
-                      fontSize: 16,
-                      height: 1.5,
-                      fontWeight: result is TracerouteStepFinished ? FontWeight.bold : FontWeight.normal,
+        : Expanded(
+            child: ListView.builder(
+              itemCount: traceResults.length,
+              itemBuilder: (context, index) {
+                final TracerouteStep? response = traceResults[index];
+                String? ipv4address = extractIPAddress(response.toString());
+
+                return Column(
+                  children: [
+                    ListTile(
+                      onTap: () {
+                        Clipboard.setData(
+                          ClipboardData(text: '$ipv4address'),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('IP copied to clipboard'),
+                          ),
+                        );
+                      },
+                      dense: true,
+                      contentPadding:
+                          const EdgeInsets.only(left: 10.0, right: 10.0),
+                      title: Text(
+                        '${response}',
+                        style: TextStyle(fontSize: 15),
+                      ),
                     ),
-                  ),
-              ],
-            );    
+                    const Divider(height: 4),
+                  ],
+                );
+              },
+            ),
+          );
   }
-  
+
   @override
   String buttonLabel() {
     // TODO: implement buttonLabel
-    return start ? 'Start' : 'Stop';    
+    return start ? 'Start' : 'Stop';
   }
-  
+
   @override
   String fieldLabel() {
     // TODO: implement fieldLabel
-    return 'Enter a domain or IP';    
+    return 'Enter a domain or IP';
   }
-  
+
   @override
-  Future<void> onPressed() async{
+  Future<void> onPressed() async {
     // TODO: implement onPressed
-    start ? onTrace() : onStop();    
+    start ? onTrace() : onStop();
   }
-  
+
   @override
   String title() {
     // TODO: implement title
-    return 'Traceroute';    
+    return 'Traceroute';
   }
 
-    @override
+  @override
   void dispose() {
-    super.dispose();  
+    traceroute.stopTrace();
+    super.dispose();
   }
+
+  String? extractIPAddress(String line) {
+    final regex = RegExp(r'(\d{1,3}\.){3}\d{1,3}');
+    final match = regex.firstMatch(line);
+
+    if (match != null) {
+      final ip = match.group(0);
+      if (ip != null) {
+        ipv4addresses.add(ip);
+      }
+      return ip;
+    }
+    return null;
+  }
+
 }
